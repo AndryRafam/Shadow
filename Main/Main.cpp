ef4f5a2fcc5ec4c52b47355963a9ff97d9ca231f
Andry Rafam Andrianjafy <andryrafam@protonmail.com>
2021-02-12 22:59:01 +0300

    Add files via upload

---
+++ Main/Main.cpp
@@ -0,0 +1,15 @@
+#ifdef __linux__
+
+#include <iostream>
+
+#include "../Core/SHADOW.h"
+#include "../Core/ASERP.h"
+
+
+int main(int argc, char** argv){
+	SHADOW sh;
+	sh.run();
+	return 0;
+}
+
+#endif
