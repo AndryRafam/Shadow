cc63bd5e1715ac8a8743449012b08c5be367e0e6
Andry Rafam Andrianjafy <andryrafam@protonmail.com>
2021-03-04 21:38:00 +0300

    Update ASERP.h

--- Core/ASERP.h
+++ Core/ASERP.h
@@ -7,7 +7,7 @@ class ASERP
 {
 	public:
 		ASERP(void);
-		std::string aserp(std::string clr_msg, std::string password, std::string salt, std::string choice);
+		std::string aserp(std::string clr_msg, std::string password1, std::string salt1, std::string password2, std::string salt2, std::string choice);
 		~ASERP(void);
 };
 
