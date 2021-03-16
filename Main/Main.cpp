53dedb63c345e5e140a5da4289ae7751873f15fa
Andry Rafam Andrianjafy <andryrafam@protonmail.com>
2021-03-16 16:46:56 +0300

    Update Main.cpp

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -6,18 +6,13 @@
 #include "../Core/Shadow.h"
 
 int main(int argc, char **argv){
-	Shadow s;
-	if(argc != 2 or (std::string(argv[1])!="encrypt" && std::string(argv[1])!="decrypt")){
-		std::cout << argv[0] << " <encrypt>: encryption mode" << "\n";
-		std::cout << argv[0] << " <decrypt>: decryption mode" << "\n";
-		exit(0);
-	}
-	if(std::string(argv[1])=="encrypt"){
-		s.encrypt(std::string(argv[1]));
+	if(argc==1){
+		Shadow s;
+		s.run();
 	}
 	else{
-		s.decrypt(std::string(argv[1]));
-	}	
+		std::cout << "Usage: " << argv[0] << " <no argument>" << "\n";
+	}
 	return 0;
 }
 
